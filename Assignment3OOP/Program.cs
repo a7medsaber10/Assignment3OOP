8f426b036588aebdb6fdcbf2b6229be3edc92ebc
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-07-14 16:51:31 +0300

    Polymorphism defination

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Assignment3OOP
+﻿using System;
+
+namespace Assignment3OOP
 {
     internal class Program
     {
+        #region Polymorphism
+        // Polymorphism can be achieved through overriding and overloading.
+        // * Allows methods to be reused in different contexts with the same name
+        // * Makes it easier to introduce new classes that conform to existing interfaces.
+        // * Reduces the need for large conditional statements by allowing different object types to be treated uniformly.
+        #endregion
+
+
         static void Main(string[] args)
         {
             
