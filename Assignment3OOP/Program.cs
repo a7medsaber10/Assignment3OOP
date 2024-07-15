4087934ed08b92da88851153501140bdb94688aa
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-07-15 22:53:08 +0300

    Overloading explaination

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Buffers.Text;
+using System.Numerics;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment3OOP
 {
@@ -10,6 +14,36 @@ namespace Assignment3OOP
         // * Makes it easier to introduce new classes that conform to existing interfaces.
         // * Reduces the need for large conditional statements by allowing different object types to be treated uniformly.
         #endregion
+
+        #region Overloading
+        // Method overloading:
+        // - Allows a class to have multiple methods with the same name but different parameters.
+        // - The compiler determines which method to invoke based on the method signatures at compile time.
+        public class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            public double Add(double a, double b)
+            {
+                return a + b;
+            }
+
+            public int Add(int a, int b, int c)
+            {
+                return a + b + c;
+            }
+        }
+
+        // The three methods with same name but different Parameters 
+        #endregion
+
+
+
+
+
 
 
         static void Main(string[] args)
